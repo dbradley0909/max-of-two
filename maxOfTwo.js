fd2d51631fe00383560ebc234e7746d0307f6290
Dana Bradley <dbradley0909@outlook.com>
2021-11-04 22:20:18 -0400

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,17 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+let x = 1
+let y = 2
+
+// Write your code below this line.
+x = 2
+y = 1
+
+function biggerNum(){
+   console.log(y+y)
+}
+
+console.log(biggerNum())
